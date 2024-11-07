f19d4b4191a2f2f46a3bee08e08a6d6559ce4e3b
Juan Freire <164571554+Clarfrecito@users.noreply.github.com>
2024-11-07 19:19:13 -0300

    Add files via upload

---
+++ SnakeGameProgramacion/SnakeGameProgramacion/src/Paquete/pintor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquete;
+
+import java.awt.Color;
+import java.awt.Image;
+
+/**
+ *
+ * @author PC01
+ */
+class pintor {
+
+    static void drawImage(Image imagen, int i, int i0, int i1, int i2, Color BLACK, PanelSnake aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void drawImage(Image cabezaImage, int i, int i0, int i1, int i2, PanelSnake aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
